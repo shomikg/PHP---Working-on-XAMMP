622f94f4a853d67ae3e81c2382d0443743675a8a
Shomik Goswami <shomikprogrammer@gmail.com>
2018-06-15 17:07:18 +0530

    for loop

---
+++ for_loop/index.php
@@ -0,0 +1,23 @@
+<?php
+
+for ($num=1;$num<=10;$num++) {
+    echo $num.'<br>';
+}
+
+echo '<br>Reverse the order:<br>';
+
+// Reverse the order
+
+for ($num=10;$num>=1;$num--) {
+    echo $num.'<br>';
+}
+
+// 2 times table
+
+echo '<br>Two Times Table:<br>';
+
+for ($num=2;$num<=20;$num=$num+2) {
+    echo $num.'<br>';
+}
+
+?>
