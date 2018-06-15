639427c397e8149d38cd84a07bdda3ed4d634657
Shomik Goswami <shomikprogrammer@gmail.com>
2018-06-15 15:42:49 +0530

    while loops

---
+++ while_loops/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$num = 10;
+
+while ($num>=0) {
+    echo $num.'<br>';
+    $num--;
+}
+
+// Another form of the while loop
+
+/*$num = 10;
+
+while ($num>=0):
+    echo $num.'<br>';
+    $num--;
+endwhile;*/
+
+?>
