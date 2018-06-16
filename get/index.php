26a087bbe91e78da34a572073838e3b90f277d77
Shomik Goswami <shomikprogrammer@gmail.com>
2018-06-16 12:54:54 +0530

    made  a cool form to get $_GET values

--- get/index.php
+++ get/index.php
@@ -1,7 +1,16 @@
+<form action="index.php" method="get">
+    Name: <br /><input type="text" name="name"><br />
+    Age: <br /><input type="text" name="age" size="5"><br /><br />
+    <input type="submit" value="Submit">
+</form>
+
+
 <?php
 
 $name = $_GET['name'];
 $age = $_GET['age'];
-
-echo 'I am '.$name.' and I am '.$age.' years old.';
+if(isset($name)&&isset($age))
+{
+    echo 'I am '.$name.' and I am '.$age.' years old.';
+}
 ?>
