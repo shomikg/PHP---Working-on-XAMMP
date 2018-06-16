0c657115dd10f0028dfa43826691d164936e316e
Shomik Goswami <shomikprogrammer@gmail.com>
2018-06-16 16:58:46 +0530

    Embedding everything finally

--- embedding/index.php
+++ embedding/index.php
@@ -1,8 +1,29 @@
+<?php
+if(isset($_POST['name']))
+{
+ $name= $_POST['name'];
+    if(!empty($name))
+    {
+        $sentence = $name.' woke up, and made a Pancake'
+    }
+    else
+    {
+        echo 'Please enter a name.';
+    }
+}
+
+?>
+
 <html>
 <head>
     
 </head>
 <body>
-    <strong><?php echo 'Shomik'; ?> </strong>
+<form action="index.php" method="POST">
+    Type your name : <br><input type="text" name="name" value="<?php echo $name ?>"><br />
+    <input type="submit" value="Submit">
+</form>
+
+<textarea rows="7" cols="25"><?php echo $sentence; ?></textarea>
 </body>
 </html>
