8e530cf2ad62ad7217064df67e4d75c90bcec352
Shomik Goswami <shomikprogrammer@gmail.com>
2018-06-16 14:19:10 +0530

    Wrote a password form to validate through $_POST

--- post/index.php
+++ post/index.php
@@ -1,3 +1,8 @@
+<form action="index.php" method="POST">
+    Please enter your password: <br />
+    <input type="password" name="password"><br />
+    <input type="submit" value="submit">
+</form>
 <?php
 
 ?>
