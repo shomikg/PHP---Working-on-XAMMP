fb976f1faf01613eff0113134849e8caef2098ab
Shomik Goswami <shomikprogrammer@gmail.com>
2018-06-16 17:01:49 +0530

    Starter file

---
+++ htmldisplay/index.php
@@ -0,0 +1,14 @@
+<?php
+$name = "alex";
+if($name == "alex")
+{
+    echo "Hi, Alex.";
+}
+else
+{
+    echo "
+    You're not Alex? Please type your name: <br />
+    <input type='text' name='name'><input type='submit' value='Submit'>
+    "
+}
+?>
