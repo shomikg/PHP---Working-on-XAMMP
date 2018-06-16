1a15fb34dd0e621616c3cc244e8a3f9a49412a26
Shomik Goswami <shomikprogrammer@gmail.com>
2018-06-16 14:21:46 +0530

    validating through $_POST

--- post/index.php
+++ post/index.php
@@ -3,6 +3,11 @@
     <input type="password" name="password"><br />
     <input type="submit" value="submit">
 </form>
-<?php
 
+<?php
+$password = 'password';
+if(isset($_POST['password'])&&empty($_POST['password']))
+{
+    echo 'submitted and filled.';
+}
 ?>
