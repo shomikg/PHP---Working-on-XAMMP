18afabe4f75c6ba00d21c8e38227a31c70d9b134
Shomik Goswami <shomikprogrammer@gmail.com>
2018-06-17 15:07:04 +0530

    checkbox validation using $_POST

---
+++ checkboxes/index.php
@@ -0,0 +1,4 @@
+<form action="process.php" method="get">
+    <p><input type="checkbox" name="agree"/> I agree with the Terms & conditions</p>
+    <p><input type="submit" value="Register" /></p>
+</form>
---
+++ checkboxes/process.php
@@ -0,0 +1,8 @@
+<?php
+if (isset($_POST['agree'])&& $_POST['agree'] == 'true')
+{
+    echo 'Set, and correct value.';
+}
+else
+    echo 'You must agree to the Terms & Conditions';
+?>
